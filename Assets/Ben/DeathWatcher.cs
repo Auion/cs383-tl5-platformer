ba043a023f208dfea3202d211146142c23386383
Benjamin R Kopf <ben.kopf1371@gmail.com>
2025-02-26 22:33:18 -0800

    Added Death Menu

---
+++ Assets/Ben/DeathWatcher.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathWatcher : MonoBehaviour
+{
+    
+    void Update()
+    {
+        PlayerScript player = FindAnyObjectByType<PlayerScript>();
+        if (player.gameOver)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}
