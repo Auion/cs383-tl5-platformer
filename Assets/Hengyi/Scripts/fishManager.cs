3a6583a2f6ab731a3b092ce35985cc2a54eca1e4
Thyme-mew <tianhengyi@hotmail.com>
2025-02-26 18:26:44 -0800

    Update Collectables
    Update Collectables

---
+++ Assets/Hengyi/Scripts/fishManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishManager : MonoBehaviour
+{
+    int fruitCount = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Fish")) 
+        { 
+            Destroy(collision.gameObject);
+            fruitCount ++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 30;
+        GUI.Label(new Rect(20, 20, 500, 500), "Fish Num: " + fruitCount);
+    }
+}
